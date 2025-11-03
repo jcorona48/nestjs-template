17c847094a72d94cde0aebd3aae6ba67d00d43aa
Joan Corona <joaneliasc86@gmail.com>
2025-11-03 16:03:44 -0400

    feat: ignore seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore-file
 import { PrismaClient } from '@prisma/client';
 import { PrismaLibSQL } from '@prisma/adapter-libsql';
 
